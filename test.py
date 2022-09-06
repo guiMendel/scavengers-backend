f6c0fa6d765ccfab92221f1668fe2e4085804597
guimendel <guimendeln@gmail.com>
2022-09-06 09:53:06 -0300

    Set up websocket server for agents

--- test.py
+++ test.py
@@ -2,26 +2,47 @@ import asyncio
 import websockets
 from datetime import datetime
 
+# Maps each agent id to it's websocket connection
+scavengers = {}
 
+# Helper to print with a timestamp
 def log(message):
     print(f"[{datetime.now().time()}]> {message}")
 
 
 async def connection_handler(websocket):
-    log(f"New connection with {websocket.id}: {websocket.local_address}")
+    log(f"Connection with {websocket.id}")
+
+    # FOR TEST PURPOSES ONLY
+    await websocket.send("hullo")
+
+    # Get agent id
+    id = await websocket.recv()
+
+    log(f"{id} connected")
+
+    # Register it
+    scavengers[id] = websocket
 
     try:
         async for message in websocket:
-            log(f"Received message: {message}")
+            log(f"Received message from {id}: {message}")
+            websocket.send("random")
 
     finally:
-        log(f"Closing connection with {websocket.id}")
+        log(f"{id} disconnected")
+
+        # Unregister it
+        scavengers.pop(id)
 
 
 async def main():
-    async with websockets.serve(connection_handler, "localhost", 3001):
-        log("Server listening")
-        await asyncio.Future()
+    try:
+        async with websockets.serve(connection_handler, "localhost", 3001):
+            log("Server listening")
+            await asyncio.Future()
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
