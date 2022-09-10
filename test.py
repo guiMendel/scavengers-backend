65c9d4e5d7438b58768d33e4c6b03444744895f6
guimendel <guimendel@gmail.com>
2022-09-10 13:50:11 -0300

    🔨Update message protocol

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import asyncio
 import websockets
+import json
 from datetime import datetime
 
 host = "localhost"
 port = 3001
 
-# Maps each agent id to it's websocket connection
+# Maps each agent id to it's knowledge base
 scavengers = {}
 
 # Helper to print with a timestamp
@@ -13,30 +14,42 @@ def log(message):
     print(f"[{datetime.now().time()}]> {message}")
 
 
+# Handles a message. The returned stirng should be the response, unless it's null
+def handle_message(message):
+    # Identify message type
+    if "connect" in message:
+        log(f"{message['id']} connected")
+
+        # Register it
+        scavengers[message['id']] = {}
+
+    if "request" in message:
+        return "random"
+
+
 async def connection_handler(websocket):
-    log(f"Connection with {websocket.id}")
+    log("App connected")
 
-    # FOR TEST PURPOSES ONLY
-    await websocket.send("hullo")
-
-    # Get agent id
-    id = await websocket.recv()
-
-    log(f"{id} connected")
-
-    # Register it
-    scavengers[id] = websocket
-
+    # Start listening for messages
     try:
-        async for message in websocket:
-            log(f"Received message from {id}: {message}")
-            websocket.send("random")
+        async for raw_message in websocket:
+            message = json.loads(raw_message)
+            
+            # log(f"Received message from {message['id']}: {message}")
+            response = handle_message(message)
+
+            if response is not None:
+                await websocket.send(response)
+
+    # Ignore connection close errors
+    except websockets.exceptions.ConnectionClosedError:
+        pass
 
     finally:
-        log(f"{id} disconnected")
+        log("App disconnected")
 
-        # Unregister it
-        scavengers.pop(id)
+        # Unregister agents
+        scavengers.clear()
 
 
 async def main():
